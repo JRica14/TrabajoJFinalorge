7985d55cecf36530696abbb444614c56212fb8b5
j.rica <j.rica@spmgm.com>
2023-05-09 15:43:20 +0200

    Actualización de diseño para ser compatible con mi movil y Ejemplo de select

--- app/src/main/java/com/example/trabajojfinalorge/Formulario.kt
+++ app/src/main/java/com/example/trabajojfinalorge/Formulario.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.trabajojfinalorge.databinding.ActivityFormularioBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class Formulario : AppCompatActivity() {
@@ -11,6 +12,9 @@ class Formulario : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityFormularioBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val db = FirebaseFirestore.getInstance()
+        val ejer = db.collection("Ejercicios")
 
         binding.inicio.setOnClickListener {
             val intent= Intent(this, Inicio::class.java)
@@ -21,5 +25,12 @@ class Formulario : AppCompatActivity() {
             startActivity(intent)
         }
 
+        /*
+        ejer.whereEqualTo("Tipo","Luxación").get().addOnSuccessListener {
+            for(documentos in it){
+                documentos['Ubicación']
+            }
+        }
+        */
     }
 }
